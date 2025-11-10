93a39866afaccd65e4d55fd6240d359cdf09eca1
Svitlana Hranovska <little.vsesvit@gmail.com>
2025-11-10 23:13:17 +0000

    corrected eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig, globalIgnores} from "eslint/config";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  globalIgnores ("**/test/")
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    // Correct key in flat config
+    ignores: ["**/test/"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
 ]);
