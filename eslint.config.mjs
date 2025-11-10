9c9c6b5ada38be697a3bf2d39257e468f77663e3
Svitlana Hranovska <little.vsesvit@gmail.com>
2025-11-10 21:19:35 +0000

    fix the conflict with ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores} from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  globalIgnores ("**/test/")
 ]);
